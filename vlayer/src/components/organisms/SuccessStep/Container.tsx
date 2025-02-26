4ab14ee47f0ad679a8f7806c0cc9bae261a21d5a
Maciej Lotkowski <maciej.lotkowski@gmail.com>
2025-02-25 17:44:40 -0700

    Show the state from the contrac

--- vlayer/src/components/organisms/SuccessStep/Container.tsx
+++ vlayer/src/components/organisms/SuccessStep/Container.tsx
@@ -1,18 +1,26 @@
-import { useSearchParams } from "react-router";
 import { useAccount } from "wagmi";
 import { SuccessStepPresentational } from "./Presentational";
+import webProofProofVerifier from "../../../../../out/WebProofVerifier.sol/WebProofVerifier.json";
+import { useReadContract } from "wagmi";
+
+
+
 
 export const SuccessStep = () => {
-  const { chain } = useAccount();
-  const [searchParams] = useSearchParams();
-  const tx = searchParams.get("tx") || "";
-  const handle = searchParams.get("handle") || "";
+  const verifierAddress = import.meta.env.VITE_VERIFIER_ADDRESS;
+  const { address } = useAccount();
+
+  const { data: isAdult } = useReadContract({
+    address: verifierAddress,
+    abi: webProofProofVerifier.abi,
+    functionName: "adults",
+    args: [address],
+  });
 
   return (
     <SuccessStepPresentational
-      tx={tx}
-      handle={handle}
-      blockExplorer={chain?.blockExplorers?.default?.url}
+      isAdult={isAdult as boolean}
+      address={address as `0x${string}`}
     />
   );
 };
--- vlayer/src/components/organisms/SuccessStep/Presentational.tsx
+++ vlayer/src/components/organisms/SuccessStep/Presentational.tsx
@@ -1,26 +1,19 @@
 import { Link } from "react-router";
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
+
 export const SuccessStepPresentational = ({
-  tx,
-  handle,
-  blockExplorer,
+  isAdult,
+  address,
 }: {
-  tx: string;
-  handle: string;
-  blockExplorer?: string;
+  isAdult: boolean;
+  address: string;
 }) => {
   return (
     <>
       <p className="text-gray-500">
-        <a
-          href={`${blockExplorer}/tx/${tx}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-violet-500 underline"
-        >
-        </a>
+        {isAdult ? "You are an adult" : "You are not an adult"}
       </p>
       <p className="text-gray-500">
+        {address}
       </p>
       <div className="mt-2 flex justify-center">
         <Link target="_blank" to="https://www.frisco.pl/stn,akcja-strefa_atrakcyjnych_cen_G3_2025/n,strefa_atrakcyjnych_cen_G3_2025?_gl=1*104dkak*_up*MQ..*_ga*OTcyMTkuMTc0MDUyODQ4NA..*_ga_N01VKB5M2C*MTc0MDUyODQ4My4xLjAuMTc0MDUyODQ4My4wLjAuMA..&kat165" id="nextButton">
